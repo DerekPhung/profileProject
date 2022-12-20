85ba2ba34dc929757488a76bfa05a4db9ee8d4ef
TubbieTubTub <derekphung16@gmail.com>
2022-12-20 17:51:01 -0500

    This is how you clump data into a receipt number

--- controller.js
+++ controller.js
@@ -26,8 +26,13 @@ class Profile{
     }
 }
 
+//api
+let url = "https://da9a-2600-1700-3f90-c170-1ca2-80a3-f726-a7ff.ngrok.io/Order";
+
 //made for arrays of objects profile
-let profileList = [];
+let profileList = [
+    // {"name":"Jerry","age":26,"gender":"male"}
+];
 
 function loadProfile(){
     document.getElementById("profileBox").innerHTML = `
@@ -44,7 +49,7 @@ function loadProfile(){
 document.getElementById("profilerCenter").innerHTML += `
     <div>
         <button id="addbtn" onclick="addInfo()">Add</button>
-        <button id="submitbtn" onclick="editInterface()">test</button>
+        <button id="submitbtn" onclick="checkout()">Checkout</button>
     </div>
 `;
 }
@@ -56,12 +61,14 @@ function addInfo(){
     let age = document.getElementById("age").value;
     let gender = document.getElementById("gender").value;
 
-    if(name.length < 1){
+    if(name.length === 0){
         alert("You forgot to enter a name!");
         return;
     }
 
-    profileList.push( new Profile(name, age, gender));
+    // profileList.push( new Profile(name, age, gender));
+    let profile = new Profile(name, age, gender);
+    profileList.push(profile);
 
     
     console.log(profileList);
@@ -70,13 +77,40 @@ function addInfo(){
     
 }
 
+function checkout(){
+
+
+    //post
+    fetch(url,{
+        method:"POST",
+        body: JSON.stringify(profileList),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    }).then(response => response.json)
+    .then(json => console.log(json));
+
+    document.getElementById("profilerCenter").innerHTML = `
+        <h1>You have successfully checked out</h1>
+    `;
+    alert("You have successfully checked out");
+
+    let profileList2 = [];
+    profileList = profileList2;
+    sideBarUpdate();
+
+}
+
+
 
 function sideBarUpdate(){
     document.getElementById("sideBar").innerHTML = "";
     for(let i = 0; i < profileList.length; i++){
         document.getElementById("sideBar").innerHTML += `
         <div class="profileAdded">
-          ${profileList[i].name}, ${profileList[i].age}, ${profileList[i].gender}
+          Name: ${profileList[i].name} <br/> 
+          Age: ${profileList[i].age} <br/>
+          Gender: ${profileList[i].gender}
           <div>
           <button class="sidebtn" onclick="deleteProfile(${i})">X</button>
           <button class="sidebtn" onclick="editInterface(${i})">edit</button>
@@ -144,7 +178,7 @@ function loadNav(){
     document.getElementById("navBar").innerHTML += `
         <div class="navItem" onclick="loadHome()">Home</div>
         <div class="navItem" onclick="loadProfile()">Ticket</div>
-        <div class="navItem" onclick="">Purchase History</div>
+        <div class="navItem" onclick="transactionHistory()">Transaction History</div>
         <div class="navItem" onclick="">Account</div>
     `;
 }
@@ -154,5 +188,33 @@ function loadHome(){
         <div id="welcomeMsg">Mochi was here!</div>
     `;
 }
+
+function transactionHistory(){
+    document.getElementById("profileBox").innerHTML = ``;
+
+    fetch(url).then(response => response.json()).then(transactionList => {
+        console.log("testing GET",transactionList);
+
+        for(let i = 0; i < transactionList.length; i++){
+            document.getElementById("profileBox").innerHTML += `
+            Receipt#: ${transactionList[i].orderID} <br/>
+            Name: ${transactionList[i].name} <br/>
+            Age: ${transactionList[i].age} <br/>
+            Gender: ${transactionList[i].gender} <br/>
+            <br/>
+        `;
+        }
+        
+
+
+    });
+
+    
+}
+
+
+
+
 loadHome();
 loadNav();
+sideBarUpdate();
